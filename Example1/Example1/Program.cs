374b58dedb6b73af22f907649b88e1adfc71b966
David Yates (3Cloud) <dyates@3cloudsolutions.com>
2023-06-21 20:30:41 -0700

    Initial check in

---
+++ Example1/Example1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+IConfiguration config = SetupConfiguration();
+IServiceProvider serviceProvider = RegisterDependencies(config);
+
+// var ddd = serviceProvider.GetRequiredService<>()
+
+
+
+
+static IServiceProvider RegisterDependencies(IConfiguration configuration)
+{
+    // ServiceCollection:  Requires Microsoft.Extensions.DependencyInjection
+    var collection = new ServiceCollection();
+    collection.AddSingleton(configuration);
+
+    // collection.AddScoped().AddSingleton<>
+
+    var serviceProvider = collection.BuildServiceProvider();
+
+    return serviceProvider;
+}
+
+
+static IConfiguration SetupConfiguration()
+{
+    // AddJsonFile requires:    Microsoft.Extensions.Configuration.Json NuGet package
+    // AddUserSecrets requires: Microsoft.Extensions.Configuration.UserSecrets NuGet package
+    // IConfiguration requires: Microsoft.Extensions.Configuration NuGet package (pulled in by previous NuGet)
+    // https://stackoverflow.com/a/46437144/97803
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .AddUserSecrets<Program>();  // this is optional if you don't plan on using secrets
+
+    IConfiguration config = builder.Build();
+
+    return config;
+}
